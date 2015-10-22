c6571cb62dfec771044803bfff14f249309a5815
Enrique Alejandro Allegretta <eallegretta@gmail.com>
2015-10-22 15:43:02 -0300

    Fixed jslint

--- minimum.js
+++ minimum.js
@@ -3,6 +3,7 @@
 // mm is a short-cut of the word Minimum
 // mm supports IE8+
 /*global ActiveXObject*/
+/*jslint bitwise: true*/
 'use strict';
 (function () {
 
@@ -641,16 +642,17 @@
     var UUID = (function () {
         var self = {};
         var lut = []; for (var i = 0; i < 256; i++) { lut[i] = (i < 16 ? '0' : '') + (i).toString(16); }
-        self.generate = function () {
+        self.generate = function() {
             var d0 = Math.random() * 0xffffffff | 0;
             var d1 = Math.random() * 0xffffffff | 0;
             var d2 = Math.random() * 0xffffffff | 0;
             var d3 = Math.random() * 0xffffffff | 0;
             return lut[d0 & 0xff] + lut[d0 >> 8 & 0xff] + lut[d0 >> 16 & 0xff] + lut[d0 >> 24 & 0xff] + '-' +
-              lut[d1 & 0xff] + lut[d1 >> 8 & 0xff] + '-' + lut[d1 >> 16 & 0x0f | 0x40] + lut[d1 >> 24 & 0xff] + '-' +
-              lut[d2 & 0x3f | 0x80] + lut[d2 >> 8 & 0xff] + '-' + lut[d2 >> 16 & 0xff] + lut[d2 >> 24 & 0xff] +
-              lut[d3 & 0xff] + lut[d3 >> 8 & 0xff] + lut[d3 >> 16 & 0xff] + lut[d3 >> 24 & 0xff];
-        }
+                lut[d1 & 0xff] + lut[d1 >> 8 & 0xff] + '-' + lut[d1 >> 16 & 0x0f | 0x40] + lut[d1 >> 24 & 0xff] + '-' +
+                lut[d2 & 0x3f | 0x80] + lut[d2 >> 8 & 0xff] + '-' + lut[d2 >> 16 & 0xff] + lut[d2 >> 24 & 0xff] +
+                lut[d3 & 0xff] + lut[d3 >> 8 & 0xff] + lut[d3 >> 16 & 0xff] + lut[d3 >> 24 & 0xff];
+        };
+
         return self;
     })(); // end UUID
 
