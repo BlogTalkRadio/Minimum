8c09b823942658354c7963041b9d5eaed7d3a6c4
Mariano Julio Vicario <elranu@gmail.com>
2015-10-27 16:01:17 -0300

    release 0.0.5

--- dist/minimum.js
+++ dist/minimum.js
@@ -302,88 +302,86 @@
 (function(mm) {
 
     function Callbacks() {
-        //_autoFire is when this event is executed only one time. 
+        //autoFire is when this event is executed only one time. 
         //And perhaps some handlers where attached after the event was release
-        this._autoFire = false;
-        this._listOfCallbacks = [];
-        this._bubbleErrors = true; //by default callbacks bubble's up errors
-        this._lastCaller = null;
-        this._lastArguments = null;
-    }
+        var autoFire = false,
+            listOfCallbacks = [],
+            bubbleErrors = true, //by default callbacks bubble's up errors
+            lastCaller = null,
+            lastArguments = null;
 
-    Callbacks.prototype.empty = function() {
-        this._listOfCallbacks = [];
-        return true;
-    };
-
-    Callbacks.prototype.enableBubbleUpErrors = function(bool) {
-        this._bubbleErrors = (bool === false) ? false : true; //by default true. Callbacks bubbles up errors
-    };
-
-    Callbacks.prototype.setAutoFireOnNewAdds = function(bool) {
-        this._autoFire = bool;
-    };
-
-    //add function/s to listen the event
-    Callbacks.prototype.add = function() {
-        for (var i = 0; i < arguments.length; i++) {
-            var fn = arguments[i];
-            if (fn && mm.isFunction(fn)) {
-                this._listOfCallbacks.push(fn);
-                if (this._autoFire) {
-                    this._fireToEventHandler(this._lastCaller, fn,
-                        this._lastArguments);
-                }
-            }
-        }
-    };
-
-    // .fire() will fire the event binding to this(to this callback)
-    // .fire(object) will fire event bindning to the first parameter object
-    // .fire(object, parm1, param2... etc) will fire the event binding it to the first objcect, and the 
-    // following parameters will be pass to the subscribed functions 
-    // fire(null, param1, param2.. etce)  will fire the event binding it to, the rest of the params will be passed
-    Callbacks.prototype.fire = function() {
-        var args = arguments,
-            context = this;
-        if (arguments.length > 0 && mm.isObject(args[0])) {
-            context = Array.prototype.splice.call(args, 0, 1)[0];
-        }
-        for (var i = 0; i < this._listOfCallbacks.length; i++) {
-            this._fireToEventHandler(context, this._listOfCallbacks[i], args);
-            if (this._autoFire) {
-                this._lastCaller = context;
-                this._lastArguments = args;
-            }
-        }
-    };
-
-    Callbacks.prototype.remove = function(fn) {
-        var index = mm.inArray(this._listOfCallbacks, fn);
-        if (mm.isFunction(fn) && index >= 0) {
-            this._listOfCallbacks.splice(index, 1);
+        this.empty = function() {
+            listOfCallbacks = [];
             return true;
-        }
-        return false;
-    };
+        };
 
-    Callbacks.prototype._fireToEventHandler = function(context, callback, args) {
-        try {
-            args = (args) ? args : []; //ie8 bug
-            callback.apply(context, args);
-        } catch (err) {
-            if (!this._bubbleErrors && console && console.error) {
-                if (err.stack) {
-                    console.error(err.stack);
-                } else {
-                    console.error('a function attached to Callbacks thorwed an error: ' + err);
+        this.enableBubbleUpErrors = function(bool) {
+            bubbleErrors = (bool === false) ? false : true; //by default true. Callbacks bubbles up errors
+        };
+
+        this.setAutoFireOnNewAdds = function(bool) {
+            autoFire = bool;
+        };
+
+        //add function/s to listen the event
+        this.add = function() {
+            for (var i = 0; i < arguments.length; i++) {
+                var fn = arguments[i];
+                if (fn && mm.isFunction(fn)) {
+                    listOfCallbacks.push(fn);
+                    if (autoFire) {
+                        fireToEventHandler(lastCaller, fn, lastArguments);
+                    }
                 }
-            } else if (this._bubbleErrors) {
-                throw err;
+            }
+        };
+
+        // .fire() will fire the event binding to this(to this callback)
+        // .fire(object) will fire event bindning to the first parameter object
+        // .fire(object, parm1, param2... etc) will fire the event binding it to the first objcect, and the 
+        // following parameters will be pass to the subscribed functions 
+        // fire(null, param1, param2.. etce)  will fire the event binding it to, the rest of the params will be passed
+        this.fire = function() {
+            var args = arguments,
+                context = this;
+            if (arguments.length > 0 && mm.isObject(args[0])) {
+                context = Array.prototype.splice.call(args, 0, 1)[0];
+            }
+            for (var i = 0; i < listOfCallbacks.length; i++) {
+                fireToEventHandler(context, listOfCallbacks[i], args);
+                if (autoFire) {
+                    lastCaller = context;
+                    lastArguments = args;
+                }
+            }
+        };
+
+        this.remove = function(fn) {
+            var index = mm.inArray(listOfCallbacks, fn);
+            if (mm.isFunction(fn) && index >= 0) {
+                listOfCallbacks.splice(index, 1);
+                return true;
+            }
+            return false;
+        };
+
+        function fireToEventHandler(context, callback, args) {
+            try {
+                args = (args) ? args : []; //ie8 bug
+                callback.apply(context, args);
+            } catch (err) {
+                if (!bubbleErrors && console && console.error) {
+                    if (err.stack) {
+                        console.error(err.stack);
+                    } else {
+                        console.error('a function attached to Callbacks thorwed an error: ' + err);
+                    }
+                } else if (bubbleErrors) {
+                    throw err;
+                }
             }
         }
-    };
-    //end Callbacks
+    }//end Callbacks
 
     //similar to $.callbacks
     //methods that supports: add, fire, remove and empty
