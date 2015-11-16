c897f40cfe66306159c77e95d63ea43d6184b85b
Mariano Julio Vicario <elranu@gmail.com>
2015-11-16 13:42:47 -0300

    release 0.0.9

--- dist/minimum.js
+++ dist/minimum.js
@@ -317,6 +317,7 @@
         //autoFire is when this event is executed only one time. 
         //And perhaps some handlers where attached after the event was release
         var autoFire = false,
+            isSuspended = false,
             listOfCallbacks = [],
             bubbleErrors = true, //by default callbacks bubble's up errors
             lastCaller = null,
@@ -328,11 +329,21 @@
         };
 
         this.enableBubbleUpErrors = function(bool) {
-            bubbleErrors = (bool === false) ? false : true; //by default true. Callbacks bubbles up errors
+            bubbleErrors = (bool === false) ? false : true; //by default true, Callbacks bubbles up errors
         };
 
         this.setAutoFireOnNewAdds = function(bool) {
             autoFire = bool;
+        };
+
+        //stops sending events if it is paused
+        this.suspend = function(){
+            isSuspended = true;
+        };
+
+        //start sending events again if it was paused
+        this.resume = function(){
+            isSuspended = false;
         };
 
         //add function/s to listen the event
@@ -379,8 +390,10 @@
 
         function fireToEventHandler(context, callback, args) {
             try {
-                args = (args) ? args : []; //ie8 bug
-                callback.apply(context, args);
+                if(!isSuspended){
+                    args = (args) ? args : []; //ie8 bug
+                    callback.apply(context, args);
+                }
             } catch (err) {
                 if (!bubbleErrors && console && console.error) {
                     if (err.stack) {
@@ -397,7 +410,7 @@
 
     //similar to $.callbacks
     //methods that supports: add, fire, remove and empty
-    //mm.callbacks(false); does not buuble ups errors
+    //mm.callbacks(false): does not bubble up errors
     mm.callbacks = function(bubbleUpErrors) {
         var callbacks = new Callbacks();
         callbacks.enableBubbleUpErrors(bubbleUpErrors);
